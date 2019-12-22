1f90fa36ad2e852f088fe7d652e86bea8c4330d7
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-12-21 16:54:21 -0800

    WIP Generating CoreML model from protobuf api

--- GenerateCoreMLTest/Sources/GenerateCoreMLTest/main.swift
+++ GenerateCoreMLTest/Sources/GenerateCoreMLTest/main.swift
@@ -8,16 +8,24 @@ let coreModel = CoreML_Specification_Model.with {
     $0.description_p = CoreML_Specification_ModelDescription.with {
         $0.input = [CoreML_Specification_FeatureDescription.with {
             $0.name = "dense_input"
-            
+            $0.type = CoreML_Specification_FeatureType.with {
+                $0.multiArrayType = CoreML_Specification_ArrayFeatureType.with {
+                    $0.shape = [1, 1]
+                    $0.dataType = CoreML_Specification_ArrayFeatureType.ArrayDataType.double
+                }
+            }
         }]
         $0.output = [CoreML_Specification_FeatureDescription.with {
             $0.name = "Identity"
-
-            
+            $0.type = CoreML_Specification_FeatureType.with {
+                $0.multiArrayType = CoreML_Specification_ArrayFeatureType.with {
+                    $0.dataType = CoreML_Specification_ArrayFeatureType.ArrayDataType.double
+                }
+            }
         }]
-//         $0.metadata.with {
-//             $0.userDefined
-//         }
+        $0.metadata = CoreML_Specification_Metadata.with {
+            $0.userDefined = ["coremltoolsVersion" : "3.1"]
+        }
     }
 }
 
